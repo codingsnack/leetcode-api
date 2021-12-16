709cb3898369a71e09b8881bc2520605acd5a585
rohitkrishna094 <rohitkrishna094@gmail.com>
2021-12-16 02:22:35 -0500

    Add GraphQLHelper class

---
+++ src/graphql-helper.ts
@@ -0,0 +1,51 @@
+import { gql, GraphQLClient } from 'graphql-request';
+import { Constants } from './constants';
+
+export class GraphQLHelper {
+  private graphQLClient: GraphQLClient;
+
+  constructor(csrfToken: string, session: string) {
+    this.graphQLClient = this.initGraphQLClient(csrfToken, session);
+  }
+
+  initGraphQLClient(csrfToken: string, session: string): GraphQLClient {
+    return new GraphQLClient(Constants.API_URL, {
+      headers: {
+        referer: Constants.ENDPOINT,
+        [Constants.X_CSRFTOKEN_HEADER_KEY]: csrfToken,
+        Cookie: this.generateCookie(csrfToken, session),
+      },
+    });
+  }
+
+  generateCookie(csrfToken: string, session: string): string {
+    return `csrftoken=${csrfToken};LEETCODE_SESSION=${session}`;
+  }
+
+  async getProblem(titleSlug: string) {
+    const variables = { titleSlug };
+    const query = gql`
+      query questionData($titleSlug: String!) {
+        question(titleSlug: $titleSlug) {
+          questionId
+          questionFrontendId
+          title
+          titleSlug
+          content
+          isPaidOnly
+          difficulty
+          likes
+          dislikes
+          isLiked
+          similarQuestions
+          exampleTestcases
+          companyTagStats
+          stats
+          hints
+          sampleTestCase
+        }
+      }
+    `;
+    return await this.graphQLClient.request(query, JSON.stringify(variables));
+  }
+}
--- src/leetcode.ts
+++ src/leetcode.ts
@@ -1,6 +1,5 @@
-import { Constants } from './constants';
-import { Problem, IProblem } from './problem';
-import { gql, GraphQLClient } from 'graphql-request';
+import { GraphQLHelper } from './graphql-helper';
+import { Problem } from './problem';
 
 interface Config {
   csrfToken: string;
@@ -8,56 +7,15 @@ interface Config {
 }
 
 export default class Leetcode {
-  private csrfToken: string;
-  private session: string;
-  private graphQLClient: GraphQLClient;
+  private graphQLHelper: GraphQLHelper;
 
   constructor(config: Config) {
     const { csrfToken, session } = config;
-    this.csrfToken = csrfToken;
-    this.session = session;
-    this.graphQLClient = this.initGraphQLClient(csrfToken, session);
-  }
-
-  initGraphQLClient(csrfToken: string, session: string): GraphQLClient {
-    return new GraphQLClient(Constants.API_URL, {
-      headers: {
-        referer: Constants.ENDPOINT,
-        [Constants.X_CSRFTOKEN_HEADER_KEY]: csrfToken,
-        Cookie: this.generateCookie(csrfToken, session),
-      },
-    });
-  }
-
-  generateCookie(csrfToken: string, session: string): string {
-    return `csrftoken=${csrfToken};LEETCODE_SESSION=${session}`;
+    this.graphQLHelper = new GraphQLHelper(csrfToken, session);
   }
 
   async getProblem(titleSlug: string): Promise<Problem> {
-    const variables = { titleSlug };
-    const query = gql`
-      query questionData($titleSlug: String!) {
-        question(titleSlug: $titleSlug) {
-          questionId
-          questionFrontendId
-          title
-          titleSlug
-          content
-          isPaidOnly
-          difficulty
-          likes
-          dislikes
-          isLiked
-          similarQuestions
-          exampleTestcases
-          companyTagStats
-          stats
-          hints
-          sampleTestCase
-        }
-      }
-    `;
-    const data = await this.graphQLClient.request(query, JSON.stringify(variables));
+    const data = await this.graphQLHelper.getProblem(titleSlug);
     const { question } = data;
     return new Problem(question);
   }
